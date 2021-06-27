2602b1409508796b12e6793904985201b7581e98
TheBoringDude <iamcoderx@gmail.com>
2021-06-27 21:32:48 +0800

    add website to works

---
+++ src/components/works-container.js
@@ -0,0 +1,48 @@
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+const WorksContainer = ({ name, description, repo, website }) => {
+  return (
+    <div className="relative border border-gray-300 rounded-lg p-6 group">
+      <a
+        href={website ? website : repo}
+        target="_blank"
+        rel="noreferrer"
+        title="Visit project website"
+        className="absolute -top-1 -right-1 bg-white border p-1 rounded-lg text-blue-300 hover:text-blue-500"
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+          <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
+          <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
+        </svg>
+      </a>
+
+      <h3 className="text-2xl font-black tracking-wide text-blue-500">{name}</h3>
+
+      <p className="line-clamp-2 my-1 text-gray-700 lg:text-lg tracking-wide">{description}</p>
+
+      <div className="mt-4">
+        <a
+          href={repo}
+          target="_blank"
+          rel="noreferrer"
+          title="Visit project repository"
+          className="bg-gray-500 hover:bg-blue-500 px-6 py-2 rounded-lg text-white"
+        >
+          <FontAwesomeIcon icon={faGithub} /> repo
+        </a>
+      </div>
+    </div>
+  );
+};
+
+WorksContainer.propTypes = {
+  name: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  repo: PropTypes.string.isRequired,
+  website: PropTypes.string
+};
+
+export default WorksContainer;
--- src/pages/works.js
+++ src/pages/works.js
@@ -1,7 +1,8 @@
 import React from 'react';
-
+import Websites from '../../content/works/websites.json';
 import Layout from '../components/layout';
 import PageHeader from '../components/pageheader';
+import WorksContainer from '../components/works-container';
 
 const WorksPage = () => (
   <Layout title="Works">
@@ -9,6 +10,18 @@ const WorksPage = () => (
       title="Discover my projects"
       info="I try to code and learn on new stuff everyday. Try to check on them..."
     />
+
+    <div className="w-5/6 mx-auto">
+      <div className="py-6">
+        <h2 className="text-xl mb-5 text-gray-600 font-bold">Websites</h2>
+
+        <div className="grid grid-cols-1  sm:grid-cols-2 gap-8">
+          {Websites.map((proj, index) => (
+            <WorksContainer key={index} {...proj} />
+          ))}
+        </div>
+      </div>
+    </div>
   </Layout>
 );
 

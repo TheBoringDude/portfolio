051e781877af7e699ffd57e4cbb306e93ba7edc4
TheBoringDude <iamcoderx@gmail.com>
2021-06-28 19:05:44 +0800

    add webapps to works

--- src/pages/works.js
+++ src/pages/works.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import WebApps from '../../content/works/webapps.json';
 import Websites from '../../content/works/websites.json';
 import Layout from '../components/layout';
 import PageHeader from '../components/pageheader';
@@ -12,6 +13,16 @@ const WorksPage = () => (
     />
 
     <div className="w-5/6 mx-auto">
+      <div className="py-6">
+        <h2 className="text-xl mb-5 text-gray-600 font-bold">Web Apps</h2>
+
+        <div className="grid grid-cols-1  sm:grid-cols-2 gap-8">
+          {WebApps.map((proj, index) => (
+            <WorksContainer key={index} {...proj} />
+          ))}
+        </div>
+      </div>
+
       <div className="py-6">
         <h2 className="text-xl mb-5 text-gray-600 font-bold">Websites</h2>
 

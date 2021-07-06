ef3f76b26dc2b20f47eb8809f5700041837c0888
TheBoringDude <iamcoderx@gmail.com>
2021-07-06 14:44:17 +0800

    feat: add website screenshots
    new files:
    - `etc/screenshots.py` -> a website image grabber api, I am more comfortable in using python with this one :)

---
+++ etc/screenshots.py
@@ -0,0 +1,29 @@
+import json
+import os
+from urllib.parse import urlencode
+from urllib.request import urlretrieve
+
+ACCESS_KEY = ""
+
+all_contents = ["websites", "webapps"]
+
+
+for i in all_contents:
+    f = open(f"content/works/{i}.json")
+
+    data = json.load(f)
+
+    for j in data:
+        image_name = f"content/screenshots/{j['name']}.jpeg"
+
+        if os.path.isfile(image_name):
+            continue
+
+        print(f"Downloading `{j['name']}`")
+
+        params = urlencode(dict(access_key=ACCESS_KEY, url=j["website"]))
+
+        urlretrieve(
+            "https://api.apiflash.com/v1/urltoimage?" + params,
+            image_name,
+        )
